cc723d070bf90ecd0163ca1abc001d6958edf089
Norma <normarivasmadrid@gmail.com>
2022-01-03 15:53:50 +0100

    fix dclass of deletebtn

--- src/js/04_favorites.js
+++ src/js/04_favorites.js
@@ -2,7 +2,7 @@
 function renderFavMovies() {
   favList.innerHTML = '';
   for (const favMovie of favMoviesData) {
-    favList.innerHTML += `<li data-id=${favMovie.mal_id} class=" section__fav--list-favorite--li  js_img " >
+    favList.innerHTML += `<li data-id=${favMovie.mal_id} class=" section__fav--list-favorite--li   " >
       <img class="section__results--list--li--img favImg" src="${favMovie.image_url}" alt="" />
       <h3 class="section__results--list--li--title favTitle" >${favMovie.title}</h3>
       <button class="section__results--list--li--delete delete-btn js_deleteBtn" title="Delete movie" data-id= "${favMovie.mal_id}" >x</button>
